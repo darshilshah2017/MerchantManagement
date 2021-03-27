e71147ece96ea8772327b4b5bca226196d0ef3eb
darshil shah <darshilshah085@gmail.com>
2021-03-27 15:31:41 +0530

    added cloud config service

---
+++ MerchantManagementCloudConfig/src/main/java/com/merchant/MerchantCloudConfigApplication.java
@@ -0,0 +1,14 @@
+package com.merchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MerchantCloudConfigApplication {
+
+    public static void main(String args[]){
+        SpringApplication.run(MerchantCloudConfigApplication.class);
+    }
+}
